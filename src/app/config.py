6e8267ed3ba858e1fc4085db074aefd3b497b094
Fernando Abreu <nando@nandoLap.fra>
2024-10-05 10:29:44 +0100

    Update vars in env and config

--- src/app/config.py
+++ src/app/config.py
@@ -1,20 +1,16 @@
 # -*- coding: utf-8 -*-
-"""Main configuration file for the project.
+"""Project configuration file.
 
-*** UPDATE env.toml AND RUN `make` or `make self-test` TO SET .env ***
+*** Update `env.toml` and run `make` to set `.env`. ***
 
-All env vars values and fallback or default values to be used in the Application.
-This module must only be updated in order to:
-- Add/remove vars imported from .env
-- Update fallback values
+This module manages environment variables and fallback values:
+- Modify only to add/remove vars or to update fallback values.
+- Use the Makefile to keep `.env` updated (values in `.env` are not persistent).
 
-Using the Makefile for every command will keep .env updated:
-- Values in .env file are not persistent: update env.toml instead
-- The .env file may be deployed to Production after build
+*** `.env` should be deployed to production. ***
 
-About prettyconf, dependency responsible for fetching env vars:
-- From env vars, Application vars are set
-- Precedence is: command line var > .env > fallback value (if set)
+Regarding prettyconf, the dependency responsible for fetching environment variables, the precedence order is:
+- command-line variable > .env file > fallback value (if set)
 """
 from prettyconf import config
 from pathlib import Path
@@ -23,27 +19,23 @@ from tempfile import gettempdir
 LOG_LEVEL: str = config('LOG_LEVEL', default='INFO')
 
 APP_NAME: str = config('APP_NAME', default='App Name')
-APP_VERSION: str = config('APP_VERSION', default='1.0.0')
+APP_VERSION: str = config('APP_VERSION', default='0.1.0')
 PROJECT_NAME: str = config('PROJECT_NAME', default=APP_NAME).replace(' ', '-').lower()
-# PROJECT_DESCRIPTION: str = config('PROJECT_DESCRIPTION', default='{} v{}'.format(APP_NAME, APP_VERSION))
+PROJECT_DESCRIPTION: str = config('PROJECT_DESCRIPTION', default=APP_NAME)
 
-LOGS_DIR: str = config('LOGS_DIR', default='/tmp/{}'.format(PROJECT_NAME))
-APP_DIR: str = config('APP_DIR', default='app')
-SRC_BASE_PATH = Path(__file__).resolve().parent.as_posix()
+LOG_ROTATION_MAX_MB: float = config('LOG_ROTATION_MAX_MB', cast=float, default='9')
+LOG_MAX_ROTATED_FILES: int = config('LOG_MAX_ROTATED_FILES', cast=int, default='5')
+
 LOGS_DIR: Path = Path(config('LOGS_DIR', default=f'/{gettempdir()}/{PROJECT_NAME}')).resolve()
 APP_DIR: Path = Path(config('APP_DIR', default='app')).resolve()
 SRC_BASE_DIR: Path = Path(__file__).resolve().parent.parent
 PROJECT_ROOT_DIR: Path = SRC_BASE_DIR.parent
-
-LOG_ROTATION_MAX_MB: float = config('LOG_ROTATION_MAX_MB', cast=float, default='9')
-LOG_MAX_ROTATED_FILES: int = config('LOG_MAX_ROTATED_FILES', cast=int, default='5')
 
 HOSTNAME_CMD_PATH: str = config('HOSTNAME_CMD_PATH', default='/usr/bin/hostname')
 GREP_CMD_PATH: str = config('GREP_CMD_PATH', default='/usr/bin/grep')
 CAT_CMD_PATH: str = config('CAT_CMD_PATH', default='/usr/bin/cat')
 SENSORS_CMD_PATH: str = config('SENSORS_CMD_PATH', default='/usr/bin/sensors')
 NVIDIA_CMD_PATH: str = config('NVIDIA_CMD_PATH', default='/usr/bin/nvidia-smi')
+
 KAFKA_BROKERS: list = sorted(d for d in config('KAFKA_BROKERS', default='').split(','))
-KAFKA_PARTITIONS: int = config('KAFKA_PARTITIONS', cast=int, default='1')
-KAFKA_RETENTION_DAYS: int = config('KAFKA_RETENTION_DAYS', cast=int, default='3')
 KAFKA_TOPIC: str = config('KAFKA_TOPIC', default=PROJECT_NAME)
