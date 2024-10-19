7ecf07070d3ef0ced4a02efa3a9fdc100cf35693
Fernando Abreu <nando@nandoLap.fra>
2024-10-19 11:58:47 +0100

    Update runner after install-source

--- setup/run.bash
+++ setup/run.bash
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-PYTHONPATH="${VIRTUAL_ENV:-dependencies}:src" python -m app
+
+PYTHON_VERSION=$(python3 -V | sed "s,.* \(3\.[0-9]\+\)\..*,\1,")
+[ -z "$VIRTUAL_ENV" ] && VIRTUAL_ENV=.venv/lib/python$PYTHON_VERSION/site-packages
+[ ! -d "$VIRTUAL_ENV" ] && VIRTUAL_ENV=dependencies
+
+set -x
+PYTHONPATH="$VIRTUAL_ENV:src" python -m app
