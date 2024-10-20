632299dec0e1559a9e6b7b309b27611b23250584
Fernando Abreu <nando@nandoLap.fra>
2024-10-20 10:40:12 +0100

    Fix venv setting from the app runner

--- setup/run.bash
+++ setup/run.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VIRTUAL_ENV=${VIRTUAL_ENV:-$(poetry env info -p 2>/dev/null || echo .venv)}
+VIRTUAL_ENV=${VIRTUAL_ENV:-$(poetry env info -p 2>/dev/null || find . -type d -name '*venv' -exec realpath {} \;)}
 
 if [ -d "$VIRTUAL_ENV" ]; then
   PACKAGES_DIR="$(find "$VIRTUAL_ENV" -type d -name site-packages | tail -1)"
